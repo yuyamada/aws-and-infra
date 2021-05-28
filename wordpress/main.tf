f17fdbb72677ab10cbf5d6c5d9b870e675303cb8
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-05-28 21:02:17 +0900

    fix

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_internet_gateway" "aws_and_infra" {
-  vpc_id = aws_vpc.aws_and_infra
+  vpc_id = aws_vpc.aws_and_infra.id
 
   tags = {
     Name = "aws-and-infra-igw"
