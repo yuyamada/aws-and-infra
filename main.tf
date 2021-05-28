b33ff772c552616d1e9d1e1935fbb133c787833a
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-05-28 20:29:43 +0900

    create vpc

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "aws-and-infra-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.10.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "aws-and-infra-public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.20.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "aws-and-infra-private-subnet-1a"
+  }
+}
