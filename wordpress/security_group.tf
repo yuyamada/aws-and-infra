3affc0bb29cc420267e7e43811662b2a263450c5
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-05-30 12:54:06 +0900

    allow icmp

--- wordpress/security_group.tf
+++ wordpress/security_group.tf
@@ -6,20 +6,29 @@ resource "aws_security_group" "aws_and_infra" {
   }
 }
 
+resource "aws_security_group_rule" "ingress_ping" {
+  type              = "ingress"
+  protocol          = "icmp"
+  from_port         = -1
+  to_port           = -1
+  cidr_blocks       = [aws_vpc.aws_and_infra.cidr_block]
+  security_group_id = aws_security_group.aws_and_infra.id
+}
+
 resource "aws_security_group_rule" "ingress_ssh" {
   type              = "ingress"
+  protocol          = "tcp"
   from_port         = 22
   to_port           = 22
-  protocol          = "tcp"
   cidr_blocks       = [aws_vpc.aws_and_infra.cidr_block]
   security_group_id = aws_security_group.aws_and_infra.id
 }
 
 resource "aws_security_group_rule" "egress_all" {
   type              = "egress"
+  protocol          = "-1"
   from_port         = 0
   to_port           = 0
-  protocol          = "-1"
   cidr_blocks       = [aws_vpc.aws_and_infra.cidr_block]
   security_group_id = aws_security_group.aws_and_infra.id
 }
