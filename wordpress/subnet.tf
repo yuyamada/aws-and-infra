9305de69b5a8d4e0f946933d99c99223dc3b4601
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-05-31 23:33:18 +0900

    add seconedary private subnet

--- wordpress/subnet.tf
+++ wordpress/subnet.tf
@@ -19,3 +19,14 @@ resource "aws_subnet" "private" {
     Name = "${local.project_name}-${terraform.workspace}-private-subnet-1a"
   }
 }
+
+resource "aws_subnet" "private_sub" {
+  vpc_id                  = aws_vpc.aws_and_infra.id
+  cidr_block              = "10.0.21.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "ap-northeast-1c"
+
+  tags = {
+    Name = "${local.project_name}-${terraform.workspace}-private-subnet-1c"
+  }
+}
