1bb43049720d6f78db822ba20d106cf889b2c34a
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-05-31 09:06:56 +0900

    private ip as local variables

--- wordpress/ec2.tf
+++ wordpress/ec2.tf
@@ -1,9 +1,13 @@
+locals {
+  private_ip = "10.0.10.10"
+}
+
 resource "aws_instance" "aws_and_infra" {
   ami                    = "ami-0ca38c7440de1749a"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.aws_and_infra.id]
-  private_ip             = "10.0.10.10"
+  private_ip             = local.private_ip
   key_name               = aws_key_pair.aws_and_infra.id
 
   user_data = <<EOF
