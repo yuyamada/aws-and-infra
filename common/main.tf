61d26c0e2a285311217e2c2c0ee6f406bf5d6dec
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-06-01 21:39:55 +0900

    remote backend

--- common/main.tf
+++ common/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "yuyamada-terraform"
+    key    = "aws_and_infra/common/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
--- wordpress/main.tf
+++ wordpress/main.tf
@@ -1,7 +1,15 @@
-locals {
-  project_name = "aws-and-infra"
+terraform {
+  backend "s3" {
+    bucket = "yuyamada-terraform"
+    key    = "aws_and_infra/wordpress/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+locals {
+  project_name = "aws-and-infra"
 }
