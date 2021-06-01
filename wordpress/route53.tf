057d467af374a33e16cdf2cddf0b64559b72009d
Yu Yamada <yuyamada@MacBookPro2019.local>
2021-06-01 23:18:09 +0900

    remoet state

--- wordpress/route53.tf
+++ wordpress/route53.tf
@@ -1,5 +1,15 @@
+data "terraform_remote_state" "common" {
+  backend = "s3"
+  config = {
+    bucket = "yuyamada-terraform"
+    key    = "aws_and_infra/common/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+
 resource "aws_route53_record" "root" {
-  zone_id = "Z04197496KUGLNFLCZGL" # common/route53.tf
+  zone_id = data.terraform_remote_state.common.outputs.zone_id
   name    = "yuyamada.com"
   type    = "A"
   ttl     = 300
